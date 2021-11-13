b1309ac5cea0cdeb834f59cc9edd5be745690ad0
RIANAEH <ririhan217@gmail.com>
2021-11-14 04:25:15 +0900

    feat: add rest api - signup(post)

--- src/main/java/com/project/mnm/controller/UserController.java
+++ src/main/java/com/project/mnm/controller/UserController.java
@@ -1,15 +1,39 @@
 package com.project.mnm.controller;
 
+import com.project.mnm.domain.Response;
+import com.project.mnm.domain.User;
 import com.project.mnm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
     @Autowired
     public UserController(UserService userService){
         this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public Response signUpUser(@RequestBody User user) throws Exception {
+        Response response = new Response();
+
+        try {
+            userService.signUpUser(user);
+            response.setResponse("success");
+            response.setMessage("회원가입을 성공적으로 완료했습니다.");
+        }
+        catch (Exception e) {
+            response.setResponse("failed");
+            response.setMessage("회원가입을 하는 도중 오류가 발생했습니다.");
+            response.setData(e.toString());
+        }
+
+        return response;
     }
 }
