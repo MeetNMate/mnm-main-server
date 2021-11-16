ffc794ec0a97c92669510d7cb01a06cf36fdff05
RIANAEH <ririhan217@gmail.com>
2021-11-16 23:01:02 +0900

    refactor : separate into user and auth

--- src/main/java/com/project/mnm/controller/UserController.java
+++ src/main/java/com/project/mnm/controller/UserController.java
@@ -3,82 +3,29 @@ package com.project.mnm.controller;
 import com.project.mnm.domain.Response;
 import com.project.mnm.domain.User;
 import com.project.mnm.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
-    public List<User> get() {
+    @GetMapping("/")
+    public List<User> getAll() {
         return userService.getAllUsers();
     }
 
-    @PostMapping("/join")
-    public Response join(@RequestBody User user) {
-        Response response = new Response();
-
-        try {
-            userService.joinUser(user);
-            response.setResponse("success");
-            response.setMessage("회원가입을 성공적으로 완료했습니다.");
-        }
-        catch (Exception e) {
-            response.setResponse("failed");
-            response.setMessage("회원가입을 하는 도중 오류가 발생했습니다.");
-            response.setData(e.toString());
-        }
-
-        return response;
+    @GetMapping("/{email}")
+    public User get(@PathVariable("email") String email) {
+        return userService.getUser(email);
     }
 
-    @PostMapping("/login")
-    public Response login(@RequestBody User user) {
-        Response response = new Response();
-
-        try {
-            String jwtToken = userService.loginUser(user);
-
-            response.setResponse("success");
-            response.setMessage("로그인을 성공적으로 완료했습니다.");
-            response.setData(jwtToken);
-        }
-        catch (Exception e) {
-            response.setResponse("failed");
-            response.setMessage("로그인을 하는 도중 오류가 발생했습니다.");
-            response.setData(e.toString());
-        }
-
-        return response;
-    }
-
-    @DeleteMapping("/users/{email}")
-    public Response remove(@PathVariable("email") String email) {
-        Response response = new Response();
-
-        try {
-            userService.removeUser(email);
-
-            response.setResponse("success");
-            response.setMessage("회원 탈퇴를 성공적으로 완료했습니다.");
-        }
-        catch (Exception e) {
-            response.setResponse("failed");
-            response.setMessage("회원 탈퇴를 하는 도중 오류가 발생했습니다.");
-            response.setData(e.toString());
-        }
-
-        return response;
-    }
-
-    @DeleteMapping("adm/users/{email}")
+    @DeleteMapping("/{email}")
     public Response delete(@PathVariable("email") String email) {
         Response response = new Response();
 
--- src/main/java/com/project/mnm/service/UserService.java
+++ src/main/java/com/project/mnm/service/UserService.java
@@ -6,11 +6,7 @@ import com.project.mnm.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.sql.Timestamp;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -24,46 +20,9 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public Long joinUser(User user) throws Exception {
-        if (!userRepository.findByEmail(user.getEmail()).isEmpty())
-                throw new Exception("이미 존재하는 회원입니다.");
-
-        return userRepository.save(User.builder()
-                .email(user.getEmail())
-                .password(passwordEncoder.encode(user.getPassword()))
-                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
-                .type(false)
-                .userMatching(true)
-                .createAt(new Timestamp(System.currentTimeMillis()))
-                .build()).getId();
-    }
-
-    public String loginUser(User user) throws Exception {
-        User member = userRepository.findByEmail(user.getEmail())
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 이메일입니다."));
-
-        if (member.getRemoveAt() != null) {
-            throw new Exception("탈퇴한 사용자입니다.");
-        }
-
-        if (!passwordEncoder.matches(user.getPassword(), member.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
-        }
-
-        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
-    }
-
-    public void logoutUser(User user) {
-
-    }
-
-    public void removeUser(String email) {
-        User member = userRepository.findByEmail(email)
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 이메일입니다."));
-
-        member.setRemoveAt(new Timestamp(System.currentTimeMillis()));
-
-        userRepository.save(member);
+    public User getUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 사용자입니다."));
     }
 
     public void deleteUser(String email) {
