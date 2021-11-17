2f0c8ab600fe6cb4e29a3de36ace0b1d15975c59
moosongsong <songe08@gmail.com>
2021-11-18 02:52:05 +0900

    feat: add controller function for read HouseRules

--- src/main/java/com/project/mnm/controller/HouseRuleController.java
+++ src/main/java/com/project/mnm/controller/HouseRuleController.java
@@ -4,10 +4,7 @@ import com.project.mnm.domain.Response;
 import com.project.mnm.dto.HouseRuleInsertDto;
 import com.project.mnm.service.HouseRuleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rule")
@@ -27,6 +24,19 @@ public class HouseRuleController {
             response = new Response("success", message, houseRuleService.createHouseRule(dto));
         } catch (Exception e) {
             String message = "하우스 룰을 등록하는 도중 오류가 발생했습니다.";
+            response = new Response("failed", message, e.toString());
+        }
+        return response;
+    }
+
+    @GetMapping("/house/{id}")
+    public Response readRules(@PathVariable("id") int houseId) {
+        Response response;
+        try {
+            String message = "하우스 룰 조회를 성공적으로 완료했습니다.";
+            response = new Response("success", message, houseRuleService.findHouseRuleByHouseId(houseId));
+        } catch (Exception e) {
+            String message = "하우스 룰을 조회하는 도중 오류가 발생했습니다.";
             response = new Response("failed", message, e.toString());
         }
         return response;
