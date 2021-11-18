4e7dc00a046b29eb8927d53c9d1aa033df8de88a
RIANAEH <ririhan217@gmail.com>
2021-11-19 03:44:00 +0900

    feat: login 성공 시 uid, token 보내기

--- src/main/java/com/project/mnm/service/AuthService.java
+++ src/main/java/com/project/mnm/service/AuthService.java
@@ -6,6 +6,7 @@ import com.project.mnm.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.json.simple.JSONObject;
 
 import java.sql.Timestamp;
 import java.util.Collections;
@@ -21,6 +22,9 @@ public class AuthService {
     public Long joinUser(User user) throws Exception {
         if (!userRepository.findByEmail(user.getEmail()).isEmpty())
             throw new Exception("이미 존재하는 회원입니다.");
+
+        if (user.getPassword() == null || user.getPassword().equals(""))
+            throw new Exception("비밀번호를 입력해주세요.");
 
         return userRepository.save(User.builder()
                 .email(user.getEmail())
@@ -44,7 +48,12 @@ public class AuthService {
             throw new IllegalArgumentException("잘못된 비밀번호입니다.");
         }
 
-        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+        String token = jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("uid", member.getId());
+        jsonObject.put("token", token);
+        return jsonObject.toJSONString();
     }
 
     public void logoutUser(User user) {
