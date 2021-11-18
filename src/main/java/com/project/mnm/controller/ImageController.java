5c5ddb425b84d4120c08d051c68500c47b865489
RIANAEH <ririhan217@gmail.com>
2021-11-19 03:45:09 +0900

    style: remove import

--- src/main/java/com/project/mnm/controller/ImageController.java
+++ src/main/java/com/project/mnm/controller/ImageController.java
@@ -3,6 +3,7 @@ package com.project.mnm.controller;
 import com.project.mnm.domain.Response;
 import com.project.mnm.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
