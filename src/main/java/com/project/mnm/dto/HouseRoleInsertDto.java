718c4a2231221b7ea8b25cfa6db358c480f1d507
moosongsong <songe08@gmail.com>
2021-11-20 04:22:10 +0900

    feat: add dto for house role insert

---
+++ src/main/java/com/project/mnm/dto/HouseRoleInsertDto.java
@@ -0,0 +1,19 @@
+package com.project.mnm.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+public class HouseRoleInsertDto {
+    private long houseId;
+    private long userId;
+    private String role;
+    private int week;
+    private Timestamp startAt;
+    private int routine;
+}
