30a8ec55bc16454d8a92c5b46476f8c930fe0c56
RIANAEH <ririhan217@gmail.com>
2021-11-21 13:52:32 +0900

    feat: 채팅 관련 model 변경

---
+++ src/main/java/com/project/mnm/domain/ChattingRoom.java
@@ -0,0 +1,26 @@
+package com.project.mnm.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_chatting")
+public class ChattingRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "request_at")
+    private Timestamp requestAt;
+    @ManyToOne
+    @JoinColumn(name = "request_uid")
+    private User requestUid;
+    @Column(name = "request_success")
+    private boolean requestSuccess;
+}
--- src/main/java/com/project/mnm/domain/UserChatting.java
+++ src/main/java/com/project/mnm/domain/UserChatting.java
@@ -5,28 +5,23 @@ import lombok.*;
 import javax.persistence.*;
 import java.sql.Timestamp;
 
-@Builder
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "chattings")
-public class Chatting {
+@Table(name = "user_chatting")
+public class UserChatting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @ManyToOne
+    @JoinColumn(name = "uid")
+    private User uid;
+    @ManyToOne
     @JoinColumn(name = "cid")
     private ChattingRoom cid;
-    @ManyToOne
-    @JoinColumn(name = "uid")
-    private User user;
-    @Column(name = "message")
-    private String message;
-    @Column(name = "send_at")
-    private Timestamp sendAt;
-    @Column(name = "is_request")
-    private Boolean isRequest;
+    @Column(name = "last_access_at")
+    private Timestamp lastAccessAt;
 }
