e2378a1d38109824220ac9da79b90b57f99f0e03
moosongsong <songe08@gmail.com>
2021-11-21 21:51:57 +0900

    feat: add evaluation repository

--- src/main/java/com/project/mnm/repository/EvaluationRepository.java
+++ src/main/java/com/project/mnm/repository/EvaluationRepository.java
@@ -1,9 +1,13 @@
 package com.project.mnm.repository;
 
 import com.project.mnm.domain.Evaluation;
+import com.project.mnm.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
+    List<Evaluation> findAllByAppraisee(User appraisee);
 }
