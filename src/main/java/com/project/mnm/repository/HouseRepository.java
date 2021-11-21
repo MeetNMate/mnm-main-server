932493c88204f7c60646fb603168f597727586d8
moosongsong <songe08@gmail.com>
2021-11-21 20:43:36 +0900

    fix: annotate unused code

--- src/main/java/com/project/mnm/repository/HouseRepository.java
+++ src/main/java/com/project/mnm/repository/HouseRepository.java
@@ -1,6 +1,7 @@
 package com.project.mnm.repository;
 
 import com.project.mnm.domain.House;
+import com.project.mnm.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import java.util.List;
 @Repository
 public interface HouseRepository extends JpaRepository<House, Long> {
 
-    @Query(value = "select h from House h where h.captainId = ?1")
-    List<House> findHousesByUserId(long uid);
+//    @Query(value = "select h from House h where h.captain = ?1")
+//    List<House> findAllBy(User user);
 
 }
