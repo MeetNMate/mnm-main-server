6b75c030af65cc5ef4b74cd89c8c71bceda46741
moosongsong <songe08@gmail.com>
2021-11-21 20:34:45 +0900

    feat: add user-house repository

--- src/main/java/com/project/mnm/repository/UserHouseRepository.java
+++ src/main/java/com/project/mnm/repository/UserHouseRepository.java
@@ -5,6 +5,9 @@ import com.project.mnm.domain.User;
 import com.project.mnm.domain.UserHouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserHouseRepository extends JpaRepository<UserHouse, Long> {
     UserHouse findByUserAndHouse(User user, House house);
+    List<UserHouse> findAllByUser(User user);
 }
