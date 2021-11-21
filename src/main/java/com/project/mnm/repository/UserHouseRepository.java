63453eaadf0b5bea3ae09e76fe78267ca4861173
moosongsong <songe08@gmail.com>
2021-11-21 20:50:20 +0900

    style: add missing annotation of Repository

--- src/main/java/com/project/mnm/repository/UserHouseRepository.java
+++ src/main/java/com/project/mnm/repository/UserHouseRepository.java
@@ -4,9 +4,11 @@ import com.project.mnm.domain.House;
 import com.project.mnm.domain.User;
 import com.project.mnm.domain.UserHouse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserHouseRepository extends JpaRepository<UserHouse, Long> {
     UserHouse findByUserAndHouse(User user, House house);
     List<UserHouse> findAllByUser(User user);
