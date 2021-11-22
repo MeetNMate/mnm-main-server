7a50b5558bd37171d4d7da20040b8f1d2bf27ef5
moosongsong <songe08@gmail.com>
2021-11-23 01:47:35 +0900

    feat: 하우스 수정 및 탈퇴 컨트롤러 추가

--- src/main/java/com/project/mnm/controller/HouseController.java
+++ src/main/java/com/project/mnm/controller/HouseController.java
@@ -2,9 +2,10 @@ package com.project.mnm.controller;
 
 import com.project.mnm.domain.House;
 import com.project.mnm.domain.Response;
-import com.project.mnm.domain.User;
 import com.project.mnm.dto.HouseInsertDto;
+import com.project.mnm.dto.HouseUpdateDto;
 import com.project.mnm.service.HouseService;
+import com.project.mnm.service.UserHouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/house")
 public class HouseController {
     private final HouseService houseService;
+    private final UserHouseService userHouseService;
 
     @Autowired
-    public HouseController(HouseService houseService) {
+    public HouseController(HouseService houseService, UserHouseService userHouseService) {
         this.houseService = houseService;
+        this.userHouseService = userHouseService;
     }
 
     @PostMapping("")
@@ -65,6 +68,33 @@ public class HouseController {
             response = new Response("success", message, houseService.findHouseById(id));
         } catch (Exception e) {
             String message = "하우스 조회를 하는 도중 오류가 발생했습니다.";
+            response = new Response("success", message, e.toString());
+        }
+        return response;
+    }
+
+    @PatchMapping("/{id}")
+    public Response editHouse(@PathVariable("id") int id, @RequestBody HouseUpdateDto dto) {
+        Response response;
+        try {
+            String message = "하우스 수정을 성공적으로 완료했습니다.";
+            response = new Response("success", message, houseService.updateHouseDetail(dto, id));
+        } catch (Exception e) {
+            String message = "하우스 수정을 하는 도중 오류가 발생했습니다.";
+            response = new Response("success", message, e.toString());
+        }
+        return response;
+    }
+
+    @DeleteMapping("/{id}")
+    public Response exitHouse(@RequestHeader(value = "X-AUTH-TOKEN") String token, @PathVariable("id") int id) {
+        Response response;
+        try {
+            String message = "하우스 나가기를 성공적으로 완료했습니다.";
+            userHouseService.exitUserFromHouse(id, token);
+            response = new Response("success", message, "");
+        } catch (Exception e) {
+            String message = "하우스 나가는 도중 오류가 발생했습니다.";
             response = new Response("success", message, e.toString());
         }
         return response;
