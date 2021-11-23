b8be674f014d9fc08bcef9aa22aaa3ec12710477
moosongsong <songe08@gmail.com>
2021-11-23 09:36:58 +0900

    refactor: 하우스 생성 컨트롤러 및 서비스 추가

--- src/main/java/com/project/mnm/service/HouseService.java
+++ src/main/java/com/project/mnm/service/HouseService.java
@@ -1,16 +1,15 @@
 package com.project.mnm.service;
 
 import com.project.mnm.config.JwtTokenProvider;
-import com.project.mnm.domain.House;
+import com.project.mnm.domain.*;
 import com.project.mnm.dto.HouseInsertDto;
 import com.project.mnm.dto.HouseUpdateDto;
-import com.project.mnm.repository.ChattingRepository;
-import com.project.mnm.repository.HouseRepository;
-import com.project.mnm.repository.UserHouseRepository;
-import com.project.mnm.repository.UserRepository;
+import com.project.mnm.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,39 +17,44 @@ public class HouseService {
     private final HouseRepository houseRepository;
     private final UserHouseRepository userHouseRepository;
     private final UserRepository userRepository;
-    private final ChattingRepository chattingRepository;
+    private final ChattingRoomRepository chattingRoomRepository;
+    private final UserChattingRepository userChattingRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public HouseService(HouseRepository houseRepository, UserHouseRepository userHouseRepository, UserRepository userRepository, ChattingRepository chattingRepository, JwtTokenProvider jwtTokenProvider) {
+    public HouseService(HouseRepository houseRepository, UserHouseRepository userHouseRepository, UserRepository userRepository, ChattingRepository chattingRepository, ChattingRoomRepository chattingRoomRepository, UserChattingRepository userChattingRepository, JwtTokenProvider jwtTokenProvider) {
         this.houseRepository = houseRepository;
         this.userHouseRepository = userHouseRepository;
         this.userRepository = userRepository;
-        this.chattingRepository = chattingRepository;
+        this.chattingRoomRepository = chattingRoomRepository;
+        this.userChattingRepository = userChattingRepository;
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    public House createdHouse(HouseInsertDto houseDto) {
-//        ChattingRoom chattingRoom = chattingRoomRepository.findById(houseDto.getChattingRoomId());
-//        List<Chatting> users = chattingRepository.findByChattingRoom(chattingRoom);
-//        User user = userRepository.findById(houseDto.getCaptainId())
-//                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 사용자입니다."));
+    public House createdHouse(HouseInsertDto houseDto, String token) {
+        ChattingRoom chattingRoom = chattingRoomRepository.findById(houseDto.getChattingRoomId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 채팅방입니다."));
+        String userEmail = jwtTokenProvider.getUserPk(token);
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 사용자입니다."));
+        List<UserChatting> userChattingList = userChattingRepository.findByChattingRoom(chattingRoom);
         House house = new House();
-//        house.setDescription(houseDto.getDescription());
-//        house.setCapacity(houseDto.getCapacity());
-//        house.setCaptain(user);
-//        house.setLocation(houseDto.getLocation());
-//        House resultHouse = houseRepository.save(house);
-//
-//        UserHouse userHouse = new UserHouse();
-//        userHouse.setHouse(resultHouse);
-//        userHouse.setUser(loginUser);
-//        userHouse.setEliminated(false);
-//        Timestamp nowTime = new Timestamp(System.currentTimeMillis());
-//        userHouse.setJoinAt(nowTime);
-//        userHouse.setLastAccessAt(nowTime);
-//        userHouseRepository.save(userHouse);
-
+        house.setDescription(houseDto.getDescription());
+        house.setCapacity(houseDto.getCapacity());
+        house.setLocation(houseDto.getLocation());
+        house.setFirstCaptain(userChattingList.get(0).getUser());
+        house.setSecondCaptain(userChattingList.get(1).getUser());
+        House resultHouse = houseRepository.save(house);
+        for(UserChatting item :userChattingList){
+            UserHouse userHouse = new UserHouse();
+            userHouse.setHouse(resultHouse);
+            userHouse.setUser(item.getUser());
+            userHouse.setEliminated(false);
+            Timestamp nowTime = new Timestamp(System.currentTimeMillis());
+            userHouse.setJoinAt(nowTime);
+            userHouse.setLastAccessAt(nowTime);
+            userHouseRepository.save(userHouse);
+        }
         return house;
     }
 
