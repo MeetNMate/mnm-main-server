2600f1814e5c66581ee8592ea998c39700469873
moosongsong <songe08@gmail.com>
2021-11-24 16:43:49 +0900

    feat: 역할 관련 사용자 반환 DTO

---
+++ src/main/java/com/project/mnm/dto/UserResponseDto.java
@@ -0,0 +1,16 @@
+package com.project.mnm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class UserResponseDto {
+    private long id;
+    private String image;
+    private String name;
+}
